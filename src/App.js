5c1815694674ca0654233b076132b78aa947cfb7
Alex H <alex.holder@bigfootds.com>
2024-05-18 10:00:12 +1000

    feat!: wip of moving journal state and editing journal state to global context

--- src/App.js
+++ src/App.js
@@ -3,59 +3,59 @@ import './App.css';
 import { useLocalStorage } from 'react-use';
 import EntryParent from './components/EntryParent';
 import EntryForm from './components/EntryForm';
+import { useJournalData } from './contexts/BlogContext';
 
 function App() {
 
-  let [journalEntries, setJournalEntries] = useState([]);
-  let [storedEntries, setStoredEntries] = useLocalStorage("journalEntries", []);
+  // let someExampleGlobalState = useJournalData();
 
-  const addEntryToJournal = (title, author, content, date = Date.now(), id = null) => {
-    // let currentJournalEntries = [...journalEntries];
-    // currentJournalEntries.push({title, author, content, date});
-    // setJournalEntries(currentJournalEntries);
+  let journalEntries = useJournalData();
+  
+  const addEntryToJournal = null;
 
-    // crypto.uuid as the key
-    // use the date/datetime/milliseconds as the key
-    // combine all data into a string title+content+date 
-    // simple number as ID eg. 0, 1, 2, 
+  // const addEntryToJournal = (title, author, content, date = Date.now(), id = null) => {
+  //   // let currentJournalEntries = [...journalEntries];
+  //   // currentJournalEntries.push({title, author, content, date});
+  //   // setJournalEntries(currentJournalEntries);
 
-    if (id){
-      // UPDATE EXISTING ENTRY 
-      // find the object with the matching ID 
-      let existingEntry = journalEntries.find((entry) => entry.id === id);
-      // remove the found object from the array
-      let currentJournalEntries = journalEntries.filter((entry) => entry.id !== id);
-      // update the found object 
-      existingEntry = {
-        title, author, content, date, id
-      }
-      // add the found object back into the array
-      let updatedJournalEntries = [...currentJournalEntries, existingEntry];
-      // write array to state & localstorage 
-      setJournalEntries(updatedJournalEntries);
-      setStoredEntries(updatedJournalEntries);
-    } else {
-      // CREATE A NEW ENTRY 
-      setJournalEntries([...journalEntries, { id: journalEntries.length, title, author, content, date }]);
-      setStoredEntries([...journalEntries, { id: journalEntries.length,title, author, content, date }]);
-    }
+  //   // crypto.uuid as the key
+  //   // use the date/datetime/milliseconds as the key
+  //   // combine all data into a string title+content+date 
+  //   // simple number as ID eg. 0, 1, 2, 
+
+  //   if (id){
+  //     // UPDATE EXISTING ENTRY 
+  //     // find the object with the matching ID 
+  //     let existingEntry = journalEntries.find((entry) => entry.id === id);
+  //     // remove the found object from the array
+  //     let currentJournalEntries = journalEntries.filter((entry) => entry.id !== id);
+  //     // update the found object 
+  //     existingEntry = {
+  //       title, author, content, date, id
+  //     }
+  //     // add the found object back into the array
+  //     let updatedJournalEntries = [...currentJournalEntries, existingEntry];
+  //     // write array to state & localstorage 
+  //     setJournalEntries(updatedJournalEntries);
+  //     setStoredEntries(updatedJournalEntries);
+  //   } else {
+  //     // CREATE A NEW ENTRY 
+  //     setJournalEntries([...journalEntries, { id: journalEntries.length, title, author, content, date }]);
+  //     setStoredEntries([...journalEntries, { id: journalEntries.length,title, author, content, date }]);
+  //   }
     
 
     
-  }
+  // }
 
-  useEffect(() => {
-    setJournalEntries(storedEntries);
-
-    // return (() => {
-    //   setStoredEntries(journalEntries);
-    // });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  
 
   return (
     <div className="App">
-      {journalEntries?.length > 0 && <p>{JSON.stringify(journalEntries)}</p>}
+
+      {/* {someExampleGlobalState} */}
+
+      {/* {journalEntries?.length > 0 && <p>{JSON.stringify(journalEntries)}</p>} */}
 
       <EntryForm entryData={{}} addEntry={addEntryToJournal} /> 
 
--- src/contexts/BlogContext.jsx
+++ src/contexts/BlogContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useLocalStorage } from "react-use";
 
 
 let defaultJournalData = [
@@ -28,14 +29,26 @@ export function useJournalDispatch(){
 }
 
 
-export function BlogProvider(props) {
+export function BlogProvider({children}) {
+
+	let [journalEntries, setJournalEntries] = useState([]);
+  	let [storedEntries, setStoredEntries] = useLocalStorage("journalEntries", []);
 
 	let [exampleState, setExampleState] = useState("Hello from global level!");
 
+	useEffect(() => {
+		setJournalEntries(storedEntries);
+	
+		// return (() => {
+		//   setStoredEntries(journalEntries);
+		// });
+	  // eslint-disable-next-line react-hooks/exhaustive-deps
+	  }, []);
+
 	return (
-		<JournalDataContext.Provider value={exampleState} >
-			<JournalDispatchContext.Provider value={setExampleState}>
-				{props.children}
+		<JournalDataContext.Provider value={journalEntries} >
+			<JournalDispatchContext.Provider value={setJournalEntries}>
+				{children}
 			</JournalDispatchContext.Provider>
 		</JournalDataContext.Provider>
 	)
